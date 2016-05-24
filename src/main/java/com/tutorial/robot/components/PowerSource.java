fd655855b9539be1669eecc09588e33a4013c1fb
v-bjangid <v-bjangid@expedia.com>
2016-05-25 00:13:38 +0530

    config.properties is added

--- src/main/java/com/tutorial/robot/components/PowerSource.java
+++ src/main/java/com/tutorial/robot/components/PowerSource.java
@@ -1,16 +1,45 @@
 
 package com.tutorial.robot.components;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 import com.tutorial.robot.exceptions.OverChargedException;
 import com.tutorial.robot.exceptions.UnderChargedException;
 
 public interface PowerSource extends Component
 {
-    float MIN_POWER_LEVEL = 0;
-    float WARNING_POWER_LEVEL = 15;
-    float MAX_POWER_LEVEL = 100;
-    float POWER_CONSUMED_PER_METER = 0.02F;
-    float POWER_CONSUMED_PER_KG_PER_METER = 0.0004F;
+    Properties CONFIG = loadPowerConfiguration();
+
+    float MIN_POWER_LEVEL = getProperty("MIN_POWER_LEVEL");
+    float WARNING_POWER_LEVEL = getProperty("WARNING_POWER_LEVEL");
+    float MAX_POWER_LEVEL = getProperty("MAX_POWER_LEVEL");
+    float POWER_CONSUMED_PER_METER = getProperty("POWER_CONSUMED_PER_METER");
+    float POWER_CONSUMED_PER_KG_PER_METER = getProperty("POWER_CONSUMED_PER_KG_PER_METER");
+
+    static Properties loadPowerConfiguration()
+    {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Properties props = new Properties();
+        try (InputStream resourceStream = loader.getResourceAsStream("config.properties"))
+        {
+            props.load(resourceStream);
+            return props;
+        }
+        catch (Exception e)
+        {
+            throw new ExceptionInInitializerError("failed to load config.properties");
+        }
+    }
+
+    static float getProperty(String key)
+    {
+        if (!CONFIG.containsKey(key))
+        {
+            throw new ExceptionInInitializerError("failed to load config property " + key);
+        }
+        return Float.parseFloat(CONFIG.getProperty(key));
+    }
 
     float getPower();
 
